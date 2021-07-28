b52c6165fc7e400ef106e592c5442c162c5f2f6c
JustusMc <dev@justusm.com>
2021-07-28 16:21:20 -0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+  
+import React, { useState, Fragment } from 'react';
 
-import AddUser from "./Components/Users/AddUser";
-import UsersList from "./Components/Users/UsersList";
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -16,10 +17,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
